81a54263748700f3cb711c820f726f62b0ec3a2a
DucDV <vanducit.98@gmail.com>
2018-12-29 12:58:11 +0700

    use pure html for form

--- DemoForm/Controllers/TestController.cs
+++ DemoForm/Controllers/TestController.cs
@@ -11,16 +11,28 @@ namespace DemoForm.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //public ActionResult AddEmp(Emp emp)
+        //{
+        //    using (var dbContext = new TestEntities())
+        //    {
+        //        dbContext.Emps.Add(emp);
+        //        dbContext.SaveChanges();
+        //        return RedirectToAction("AddEmp");
+        //    }
+
+        //}
+
         [HttpPost]
-        public ActionResult AddEmp(Emp emp)
+        public ActionResult AddEmp(string name, string phoneNumber, string address)
         {
+            Emp emp = new Emp() {Address = address, Name = name, PhoneNumber = phoneNumber};
             using (var dbContext = new TestEntities())
             {
                 dbContext.Emps.Add(emp);
                 dbContext.SaveChanges();
                 return RedirectToAction("AddEmp");
             }
-
         }
     }
 }
