a07a03804601cc7861ad15a594dcf0d222f4a264
DucDV <vanducit.98@gmail.com>
2018-12-29 11:06:32 +0700

    first commit

---
+++ DemoForm/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using DemoForm.Models.Entities;
+using System.Web.Mvc;
+
+namespace DemoForm.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ViewResult AddEmp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmp(Emp emp)
+        {
+            using (var dbContext = new TestEntities())
+            {
+                dbContext.Emps.Add(emp);
+                dbContext.SaveChanges();
+                return RedirectToAction("AddEmp");
+            }
+
+        }
+    }
+}
